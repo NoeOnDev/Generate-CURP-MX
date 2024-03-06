c8b56c7d2085c2e69c85241d6fd98e18f692010a
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-06 13:04:29 -0600

    Eliminado archivo README.md y agregadas dependencias de react-router-dom y react-router. Añadido componente App.jsx para manejar rutas.

---
+++ src/Pages/generateCURP.jsx
@@ -0,0 +1,9 @@
+function GenerateCURP() {
+    return (
+        <div>
+            <h1>Generate CURP</h1>
+        </div>
+    );
+}
+
+export default GenerateCURP;
---
+++ src/container/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import GenerateCURP from "../Pages/generateCURP";
+
+function App() {
+    return  (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<GenerateCURP />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
