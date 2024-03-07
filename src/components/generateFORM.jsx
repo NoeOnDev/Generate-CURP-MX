9306a6ec9755cf0584930d577dcd282a383ca848
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-06 23:27:53 -0600

    Agrega estilos de Bootstrap y formulario de generación de CURP

--- src/components/generateFORM.jsx
+++ src/components/generateFORM.jsx
@@ -1,7 +1,21 @@
+import styles from '../assets/styles/generateForm.module.css'
+
 function GenerateCurpForm() {
     return (
-        <div>
-            Hola, soy el formulario para generar la CURP
+        <div className={styles.container}>
+            <form className={styles.form}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Nombre(s)</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text"></div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Apellidos</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <div id="passwordHelp" className="form-text"></div>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter,Route, Routes } from "react-router-dom";
-import GenerateCURP from "../Pages/generateCURP";
+import { BrowserRouter,Route, Routes } from "react-router-dom"
+import GenerateCURP from "../Pages/generateCURP"
 
 
 function App() {
