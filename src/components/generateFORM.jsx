df5c9a1d3428076e015bd39dfa9fecfecc928a53
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-07 00:14:36 -0600

    Agrega campo de selección de estado en formulario de generación de CURP

--- src/components/generateFORM.jsx
+++ src/components/generateFORM.jsx
@@ -29,19 +29,57 @@ function GenerateCurpForm() {
                         <div id="birthdateHelp" className="form-text"></div>
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">Género</label>
-                        <div className="form-check">
+                        <label className="form-label">Género</label> <br />
+                        <div className="form-check form-check-inline">
                             <input className="form-check-input" type="radio" name="gender" id="genderMale" value="M" />
                             <label className="form-check-label" htmlFor="genderMale">
                                 Hombre
                             </label>
                         </div>
-                        <div className="form-check">
+                        <div className="form-check form-check-inline">
                             <input className="form-check-input" type="radio" name="gender" id="genderFemale" value="F" />
                             <label className="form-check-label" htmlFor="genderFemale">
                                 Mujer
                             </label>
                         </div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="state" className="form-label">Estado</label>
+                        <select className="form-select" id="state">
+                            <option selected>Selecciona tu estado</option>
+                            <option value="AGUASCALIENTES">AGUASCALIENTES</option>
+                            <option value="BAJA CALIFORNIA">BAJA CALIFORNIA</option>
+                            <option value="BAJA CALIFORNIA SUR">BAJA CALIFORNIA SUR</option>
+                            <option value="CAMPECHE">CAMPECHE</option>
+                            <option value="CHIAPAS">CHIAPAS</option>
+                            <option value="CHIHUAHUA">CHIHUAHUA</option>
+                            <option value="CIUDAD DE MEXICO">CIUDAD DE MEXICO</option>
+                            <option value="COAHUILA">COAHUILA</option>
+                            <option value="COLIMA">COLIMA</option>
+                            <option value="DURANGO">DURANGO</option>
+                            <option value="ESTADO DE MEXICO">ESTADO DE MEXICO</option>
+                            <option value="GUANAJUATO">GUANAJUATO</option>
+                            <option value="GUERRERO">GUERRERO</option>
+                            <option value="HIDALGO">HIDALGO</option>
+                            <option value="JALISCO">JALISCO</option>
+                            <option value="MICHOACAN">MICHOACAN</option>
+                            <option value="MORELOS">MORELOS</option>
+                            <option value="NAYARIT">NAYARIT</option>
+                            <option value="NUEVO LEON">NUEVO LEON</option>
+                            <option value="OAXACA">OAXACA</option>
+                            <option value="PUEBLA">PUEBLA</option>
+                            <option value="QUERETARO">QUERETARO</option>
+                            <option value="QUINTANA ROO">QUINTANA ROO</option>
+                            <option value="SAN LUIS POTOSI">SAN LUIS POTOSI</option>
+                            <option value="SINALOA">SINALOA</option>
+                            <option value="SONORA">SONORA</option>
+                            <option value="TABASCO">TABASCO</option>
+                            <option value="TAMAULIPAS">TAMAULIPAS</option>
+                            <option value="TLAXCALA">TLAXCALA</option>
+                            <option value="VERACRUZ">VERACRUZ</option>
+                            <option value="YUCATAN">YUCATAN</option>
+                            <option value="ZACATECAS">ZACATECAS</option>
+                        </select>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
