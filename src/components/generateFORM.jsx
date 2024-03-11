010b9849f6db9b9596628dc82663727c7e508e7c
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-11 06:58:25 -0600

    Actualizar useFormState.jsx: Agregar función handleGenderChange y generatePDF, eliminar funciones innecesarias

--- src/components/generateFORM.jsx
+++ src/components/generateFORM.jsx
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import jsPDF from 'jspdf'
+import React from 'react';
+import jsPDF from 'jspdf';
+import useFormState from './useFormState';
 import PersonalInfoForm from './personalInfoForm';
 import StateSelection from './stateSelection';
 import AccessCodeInput from './accessCodeInput';
 import CurpGenerator from './curpGenerator';
-import styles from '../assets/styles/generateForm.module.css'
+import styles from '../assets/styles/generateForm.module.css';
 
 function GenerateCurpForm() {
-    const [curp, setCurp] = useState('');
-    const [accessCode, setAccessCode] = useState('');
-    const [inputCode, setInputCode] = useState('');
-    const [isValidCode, setIsValidCode] = useState(false);
-    const [showMessage, setShowMessage] = useState(false);
-    const [showDownloadLink, setShowDownloadLink] = useState(false);
-    const [formData, setFormData] = useState({
+    const {
+        formData,
+        curp,
+        accessCode,
+        inputCode,
+        isValidCode,
+        showMessage,
+        showDownloadLink,
+        handleClearForm,
+        handleInputChange,
+        handleGenderChange,
+        handleCodeChange,
+        handleSubmit,
+        generatePDF
+    } = useFormState({
         nombre: '',
         apellidos: '',
         dia: '',
@@ -22,195 +31,6 @@ function GenerateCurpForm() {
         genero: '',
         estado: ''
     });
-
-    const handleClearForm = () => {
-        setFormData({
-            nombre: '',
-            apellidos: '',
-            dia: '',
-            mes: '',
-            anio: '',
-            genero: '',
-            estado: ''
-        });
-        setCurp('');
-        setInputCode('');
-        setIsValidCode(false);
-        setShowMessage(false);
-        setShowDownloadLink(false);
-        setAccessCode(generateRandomCode());
-    };
-
-    const handleInputChange = (e) => {
-        const { id, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [id]: value
-        }));
-    };
-
-    const handleGenderChange = (e) => {
-        setFormData(prevState => ({
-            ...prevState,
-            genero: e.target.value
-        }));
-    };
-
-    const generateRandomCode = () => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let code = '';
-        for (let i = 0; i < 5; i++) {
-            code += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return code;
-    };
-
-    useEffect(() => {
-        const newCode = generateRandomCode();
-        setAccessCode(newCode);
-    }, []);
-
-    const handleCodeChange = (e) => {
-        setInputCode(e.target.value);
-        setIsValidCode(e.target.value === accessCode);
-    };
-
-    const estados = {
-        "AGUASCALIENTES": "AS",
-        "BAJA CALIFORNIA": "BC",
-        "BAJA CALIFORNIA SUR": "BS",
-        "CAMPECHE": "CC",
-        "CHIAPAS": "CS",
-        "CHIHUAHUA": "CH",
-        "CIUDAD DE MEXICO": "DF",
-        "COAHUILA": "CL",
-        "COLIMA": "CM",
-        "DURANGO": "DG",
-        "ESTADO DE MEXICO": "MC",
-        "GUANAJUATO": "GT",
-        "GUERRERO": "GR",
-        "HIDALGO": "HG",
-        "JALISCO": "JC",
-        "MICHOACAN": "MN",
-        "MORELOS": "MS",
-        "NAYARIT": "NT",
-        "NUEVO LEON": "NL",
-        "OAXACA": "OC",
-        "PUEBLA": "PL",
-        "QUERETARO": "QT",
-        "QUINTANA ROO": "QR",
-        "SAN LUIS POTOSI": "SP",
-        "SINALOA": "SL",
-        "SONORA": "SR",
-        "TABASCO": "TC",
-        "TAMAULIPAS": "TS",
-        "TLAXCALA": "TL",
-        "VERACRUZ": "VZ",
-        "YUCATAN": "YN",
-        "ZACATECAS": "ZS",
-        "NACIDO EN EL EXTRANJERO": "NE"
-    };
-
-    const generateCurp = ({ nombre, apellidos, dia, mes, anio, genero, estado }) => {
-        const apellidosArray = apellidos.toUpperCase().split(' ');
-        const primerApellido = apellidosArray[0] || 'X';
-        const segundoApellido = apellidosArray[1] || 'X';
-        const primerApellidoLetras = primerApellido.substr(0, 2);
-        const segundoApellidoLetra = segundoApellido.substr(0, 1);
-        const nombresArray = nombre.toUpperCase().split(' ');
-        const primerNombre = nombresArray[0] === 'MARIA' || nombresArray[0] === 'JOSE' && nombresArray.length > 1 ? nombresArray[1] : nombresArray[0];
-        const nombreLetra = primerNombre.substr(0, 1);
-        const fechaFormato = `${anio.substr(-2)}${mes.padStart(2, '0')}${dia.padStart(2, '0')}`;
-        const generoLetra = genero === 'M' ? 'H' : 'M';
-        const estadoCodigo = estados[estado.toUpperCase()] || 'NE';
-        const primeraConsonanteInterna = (str) => {
-            const match = str.substr(1).match(/[BCDFGHJKLMNPQRSTVWXYZ]/);
-            return match ? match[0] : 'X';
-        };
-        const primerApellidoConsonanteInterna = primeraConsonanteInterna(primerApellido);
-        const segundoApellidoConsonanteInterna = primeraConsonanteInterna(segundoApellido);
-        const nombreConsonanteInterna = primeraConsonanteInterna(primerNombre);
-
-        let curp = `${primerApellidoLetras}${segundoApellidoLetra}${nombreLetra}${fechaFormato}${generoLetra}${estadoCodigo}${primerApellidoConsonanteInterna}${segundoApellidoConsonanteInterna}${nombreConsonanteInterna}`;
-
-        if (curp === "ROMN031127HVZDTX") {
-            curp += "A6";
-        } else {
-            let homoclave;
-            if (parseInt(anio) >= 2000) {
-                const letraAleatoria = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-                const numeroAleatorio = Math.floor(Math.random() * 10);
-                homoclave = letraAleatoria + numeroAleatorio.toString();
-            } else {
-                homoclave = (Math.floor(Math.random() * 90) + 10).toString();
-            }
-            curp += homoclave;
-        }
-
-        return curp;
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (isValidCode) {
-            const curpData = generateCurp(formData);
-            setCurp(curpData);
-            const newCode = generateRandomCode();
-            setAccessCode(newCode);
-            setIsValidCode(false);
-            setShowMessage(false);
-            setInputCode('');
-
-            generatePDF(curpData);
-        } else {
-            setShowMessage(true);
-        }
-    };
-
-    const generatePDF = (curpData) => {
-        const pdf = new jsPDF();
-
-        const imgData = '/curp.jpg';
-        pdf.addImage(imgData, 'JPEG', 7, 12, 198, 92);
-
-        const imgCuadro = '/cuadro.png';
-        pdf.addImage(imgCuadro, 'PNG', 144.5, 35);
-
-        const imgAbajo = '/abajo.png';
-        pdf.addImage(imgAbajo, 'PNG', 7, 150, 198, 130);
-
-        pdf.setFontSize(8);
-        pdf.text('CURP Certificada: verificada con el Registro Civil', 128, 112);
-
-        pdf.setFontSize(8);
-        pdf.setFont("helvetica", "bold");
-        pdf.text(`${formData.nombre} ${formData.apellidos}`, 7, 125);
-
-        pdf.setFontSize(10);
-        pdf.setFont("helvetica", "bold");
-        pdf.text('Clave: ', 64, 52);
-
-        pdf.setFontSize(16);
-        pdf.text(`${curpData}`, 64, 59);
-
-        pdf.setFontSize(10);
-        pdf.setFont("helvetica", "bold");
-        pdf.text('Nombre: ', 64, 68);
-
-        pdf.setFontSize(16);
-        pdf.text(`${formData.nombre} ${formData.apellidos}`, 64, 75);
-
-        pdf.setFontSize(10);
-        pdf.setFont("helvetica", "bold");
-        pdf.text('Entidad de registro: ', 64, 85);
-
-        pdf.setFontSize(11);
-        pdf.text(`${formData.estado}`, 101, 85);
-
-        const pdfName = 'curp.pdf';
-        pdf.save(pdfName);
-        setShowDownloadLink(true);
-    };
 
     return (
         <div className={styles.container}>
@@ -258,7 +78,7 @@ function GenerateCurpForm() {
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default GenerateCurpForm;
--- src/components/useFormState.jsx
+++ src/components/useFormState.jsx
@@ -1,7 +1,6 @@
-// useFormState.js
 import { useState, useEffect } from 'react';
 
-export function useFormState(initialState) {
+function useFormState(initialState) {
     const [formData, setFormData] = useState(initialState);
     const [curp, setCurp] = useState('');
     const [accessCode, setAccessCode] = useState('');
@@ -28,6 +27,13 @@ export function useFormState(initialState) {
         }));
     };
 
+    const handleGenderChange = (e) => {
+        setFormData(prevState => ({
+            ...prevState,
+            genero: e.target.value
+        }));
+    };
+
     const generateRandomCode = () => {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let code = '';
@@ -47,6 +53,143 @@ export function useFormState(initialState) {
         setIsValidCode(e.target.value === accessCode);
     };
 
+    const estados = {
+        "AGUASCALIENTES": "AS",
+        "BAJA CALIFORNIA": "BC",
+        "BAJA CALIFORNIA SUR": "BS",
+        "CAMPECHE": "CC",
+        "CHIAPAS": "CS",
+        "CHIHUAHUA": "CH",
+        "CIUDAD DE MEXICO": "DF",
+        "COAHUILA": "CL",
+        "COLIMA": "CM",
+        "DURANGO": "DG",
+        "ESTADO DE MEXICO": "MC",
+        "GUANAJUATO": "GT",
+        "GUERRERO": "GR",
+        "HIDALGO": "HG",
+        "JALISCO": "JC",
+        "MICHOACAN": "MN",
+        "MORELOS": "MS",
+        "NAYARIT": "NT",
+        "NUEVO LEON": "NL",
+        "OAXACA": "OC",
+        "PUEBLA": "PL",
+        "QUERETARO": "QT",
+        "QUINTANA ROO": "QR",
+        "SAN LUIS POTOSI": "SP",
+        "SINALOA": "SL",
+        "SONORA": "SR",
+        "TABASCO": "TC",
+        "TAMAULIPAS": "TS",
+        "TLAXCALA": "TL",
+        "VERACRUZ": "VZ",
+        "YUCATAN": "YN",
+        "ZACATECAS": "ZS",
+        "NACIDO EN EL EXTRANJERO": "NE"
+    };
+
+    const generateCurp = ({ nombre, apellidos, dia, mes, anio, genero, estado }) => {
+        const apellidosArray = apellidos.toUpperCase().split(' ');
+        const primerApellido = apellidosArray[0] || 'X';
+        const segundoApellido = apellidosArray[1] || 'X';
+        const primerApellidoLetras = primerApellido.substr(0, 2);
+        const segundoApellidoLetra = segundoApellido.substr(0, 1);
+        const nombresArray = nombre.toUpperCase().split(' ');
+        const primerNombre = nombresArray[0] === 'MARIA' || nombresArray[0] === 'JOSE' && nombresArray.length > 1 ? nombresArray[1] : nombresArray[0];
+        const nombreLetra = primerNombre.substr(0, 1);
+        const fechaFormato = `${anio.substr(-2)}${mes.padStart(2, '0')}${dia.padStart(2, '0')}`;
+        const generoLetra = genero === 'M' ? 'H' : 'M';
+        const estadoCodigo = estados[estado.toUpperCase()] || 'NE';
+        const primeraConsonanteInterna = (str) => {
+            const match = str.substr(1).match(/[BCDFGHJKLMNPQRSTVWXYZ]/);
+            return match ? match[0] : 'X';
+        };
+        const primerApellidoConsonanteInterna = primeraConsonanteInterna(primerApellido);
+        const segundoApellidoConsonanteInterna = primeraConsonanteInterna(segundoApellido);
+        const nombreConsonanteInterna = primeraConsonanteInterna(primerNombre);
+
+        let curp = `${primerApellidoLetras}${segundoApellidoLetra}${nombreLetra}${fechaFormato}${generoLetra}${estadoCodigo}${primerApellidoConsonanteInterna}${segundoApellidoConsonanteInterna}${nombreConsonanteInterna}`;
+
+        if (curp === "ROMN031127HVZDTX") {
+            curp += "A6";
+        } else {
+            let homoclave;
+            if (parseInt(anio) >= 2000) {
+                const letraAleatoria = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+                const numeroAleatorio = Math.floor(Math.random() * 10);
+                homoclave = letraAleatoria + numeroAleatorio.toString();
+            } else {
+                homoclave = (Math.floor(Math.random() * 90) + 10).toString();
+            }
+            curp += homoclave;
+        }
+
+        return curp;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (isValidCode) {
+            const curpData = generateCurp(formData);
+            setCurp(curpData);
+            const newCode = generateRandomCode();
+            setAccessCode(newCode);
+            setIsValidCode(false);
+            setShowMessage(false);
+            setInputCode('');
+
+            generatePDF(curpData);
+        } else {
+            setShowMessage(true);
+        }
+    };
+
+    const generatePDF = (curpData) => {
+        const pdf = new jsPDF();
+
+        const imgData = '/curp.jpg';
+        pdf.addImage(imgData, 'JPEG', 7, 12, 198, 92);
+
+        const imgCuadro = '/cuadro.png';
+        pdf.addImage(imgCuadro, 'PNG', 144.5, 35);
+
+        const imgAbajo = '/abajo.png';
+        pdf.addImage(imgAbajo, 'PNG', 7, 150, 198, 130);
+
+        pdf.setFontSize(8);
+        pdf.text('CURP Certificada: verificada con el Registro Civil', 128, 112);
+
+        pdf.setFontSize(8);
+        pdf.setFont("helvetica", "bold");
+        pdf.text(`${formData.nombre} ${formData.apellidos}`, 7, 125);
+
+        pdf.setFontSize(10);
+        pdf.setFont("helvetica", "bold");
+        pdf.text('Clave: ', 64, 52);
+
+        pdf.setFontSize(16);
+        pdf.text(`${curpData}`, 64, 59);
+
+        pdf.setFontSize(10);
+        pdf.setFont("helvetica", "bold");
+        pdf.text('Nombre: ', 64, 68);
+
+        pdf.setFontSize(16);
+        pdf.text(`${formData.nombre} ${formData.apellidos}`, 64, 75);
+
+        pdf.setFontSize(10);
+        pdf.setFont("helvetica", "bold");
+        pdf.text('Entidad de registro: ', 64, 85);
+
+        pdf.setFontSize(11);
+        pdf.text(`${formData.estado}`, 101, 85);
+
+        const pdfName = 'curp.pdf';
+        pdf.save(pdfName);
+        setShowDownloadLink(true);
+    };
+
     return {
         formData,
         curp,
@@ -55,15 +198,13 @@ export function useFormState(initialState) {
         isValidCode,
         showMessage,
         showDownloadLink,
-        setFormData,
-        setCurp,
-        setAccessCode,
-        setInputCode,
-        setIsValidCode,
-        setShowMessage,
-        setShowDownloadLink,
         handleClearForm,
         handleInputChange,
-        handleCodeChange
+        handleGenderChange,
+        handleCodeChange,
+        handleSubmit,
+        generatePDF
     };
 }
+
+export default useFormState;
